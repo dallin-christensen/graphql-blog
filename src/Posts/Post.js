74933c4dd81d3125d9709f167a9d0734db5f1488
Dallin Christensen <dallin@pyxiscoin.com>
2018-12-23 09:07:25 -0800

    fetching singluar post with variable insertion

--- src/Posts/Post.js
+++ src/Posts/Post.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class Post extends Component {
   render() {
+    const { id } = this.props.match.params
     return (
-      <div>
-        <h1>hi</h1>
-      </div>
+      <Query
+        query={POST_QUERY}
+        variables={{ id }}
+      >
+        {({ data, loading }) => {
+          if (loading) return 'loading...'
+          const { post } = data
+          return <h1>{post.title}</h1>
+        }}
+      </Query>
     )
   }
 }
+
+const POST_QUERY = gql`
+  query post($id: ID!) {
+    post(where: { id: $id}) {
+      id
+      title
+      body
+    }
+  }
+`
 
 export default Post
